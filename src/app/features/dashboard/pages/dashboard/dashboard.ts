4e00c0a18535a22a5adba0040cc42813484c9ba3
rafenex <dvr.rafa@gmail.com>
2026-09-19 19:57:52 -0300

    feat: add logout

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,4 +12,15 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
