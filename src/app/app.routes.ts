dc60b8313ce2f64e4ed7fb8bc6787134b28c9577
rafenex <dvr.rafa@gmail.com>
2026-09-19 19:53:13 -0300

    feat: add dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+
 import { Login } from './features/auth/pages/login/login';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard
   },
   {
     path: '',
---
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss'
+})
+export class Dashboard {
+}
